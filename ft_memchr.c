a5e6549d8e4fd6e6eb01a2f949e6eada9debe241
evertonportela <evertonportela@outlook.com>
2023-05-23 21:46:32 -0300

    correção ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -16,12 +16,14 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	index;
 	char	*str;
+	char	character;
 
 	str = (char *) s;
+	character = (char) c;
 	index = 0;
 	while (index < n)
 	{
-		if (str[index] == c)
+		if (str[index] == character)
 		{
 			return ((void *)str + index);
 		}
