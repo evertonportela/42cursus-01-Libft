cbc62d87dde8ad705c94c94098bff608ccf37f02
evertonportela <evertonportela@outlook.com>
2023-05-24 19:47:34 -0300

    update - ft_lstnew cast on malloc

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 18:35:20 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/19 12:34:05 by evportel         ###   ########.fr       */
+/*   Updated: 2023/05/24 19:43:14 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*node;
 
-	node = malloc (sizeof(t_list));
+	node = (t_list *) malloc(sizeof(t_list));
 	if (!node)
 		return (NULL);
 	node->content = content;
