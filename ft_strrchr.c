25f8d3ee6ce6601fc278656e62fedc685b6ce623
evertonportela <evertonportela@outlook.com>
2023-05-26 17:47:13 -0300

    refactor - modificacoes na funcao ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: evportel <evportel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 10:35:33 by evportel          #+#    #+#             */
-/*   Updated: 2023/05/24 21:22:39 by evportel         ###   ########.fr       */
+/*   Updated: 2023/05/26 17:34:17 by evportel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,19 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*matched;
+	unsigned char	*matched;
+	unsigned char	*str;
 
+	str = (unsigned char *)s;
 	c = (unsigned char)c;
-	matched = 0;
-	while (*s)
-	{
-		if (*s == c)
-			matched = (char *)s;
-		s++;
-	}
 	if (c == '\0')
-		return ((char *)s);
-	return (matched);
+		return ((char *)(str + ft_strlen((char *)str)));
+	matched = 0;
+	while (*str)
+	{
+		if (*str == c)
+			matched = str;
+		str++;
+	}
+	return ((char *)(matched));
 }
